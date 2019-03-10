631c671b68a509c36b878e185cc0b0112434c38e
gobisa <gobisa@umich.edu>
2019-03-10 13:09:05 -0400

    made messages random rather than certain days

--- messages.py
+++ messages.py
@@ -1,13 +1,16 @@
 from messaging import send_message
+import random
 
 def LA_time():
-	send_message("9:08, LA time")
+    if random.randrange(2) == 0:
+	    send_message("9:08, LA time")
 
 def meat_show():
 	send_message("welcome to the meat show")
 
 def five_o_clock():
-	send_message("It's 5 o'clock in the morning")
+    if random.randrange(10) == 0:
+	    send_message("It's 5 o'clock in the morning")
 
 def test_scheduler():
 	send_message("scheduled message in memory of JP")
